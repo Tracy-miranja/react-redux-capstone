592ca83f722ac5d41bd91bca0b9a7e865b932fa4
Tracy-miranja <barazatracy44@gmail.com>
2023-04-26 13:08:50 +0300

    Add detail page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,17 @@
+import {  useSelector } from "react-redux";
 
-function Details(){
-    return(
-        <>
-        hellllo
-        </>
-    )
-}
+  function Details({ albumId }) {
+    const { album } = useSelector((state) => state.album);
+    const selectedAlbum = album.find((alb) => alb.Combined_Key === albumId);
+  
+    return (
+      <section>
+        <p>Confirmed cases: {selectedAlbum.Confirmed}</p>
+        <p>Deaths: {selectedAlbum.Deaths}</p>
+        <p>Recovered: {selectedAlbum.Recovered}</p>
+      </section>
+    );
+  }
+  
 
 export default Details;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fecthAlbum } from "../redux/Homepage/HomesSlice";
+import Details from "./Details";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faSkull,
@@ -11,51 +12,73 @@ import {
     faArrowRight
   } from '@fortawesome/free-solid-svg-icons';
 
-function Home() {
-  const dispatch = useDispatch();
-  const { album, isLoading } = useSelector((state) => state.album);
-  const [search, setSearch] = useState('');
-//   const [value, setValue] = useState('')
-
-  useEffect(() => {
-    dispatch(fecthAlbum());
-  }, [dispatch]);
-
-  if (isLoading) {
-    return <h3>Loading...</h3>;
+  function Home() {
+    const dispatch = useDispatch();
+    const { album, isLoading } = useSelector((state) => state.album);
+    const [search, setSearch] = useState("");
+    const [selectedAlbumId, setSelectedAlbumId] = useState(null);
+  
+    useEffect(() => {
+      dispatch(fecthAlbum());
+    }, [dispatch]);
+  
+    if (isLoading) {
+      return <h3>Loading...</h3>;
+    }
+  
+    const handleSelectAlbum = (albumId) => {
+      setSelectedAlbumId(albumId);
+    };
+  
+    const handleResetSelection = () => {
+      setSelectedAlbumId(null);
+    };
+  
+    return (
+      <section>
+        {selectedAlbumId ? (
+          <div>
+            <button onClick={handleResetSelection}>Back to list</button>
+            <Details albumId={selectedAlbumId} />
+          </div>
+        ) : (
+          <div>
+            <div className="form">
+              <form className="search-box">
+                <input
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  placeholder="Country"
+                />
+                <button type="submit">Search</button>
+              </form>
+            </div>
+            <div className="row">
+              {album
+                .filter((album) =>
+                  album.Country_Region.toLowerCase().includes(search)
+                )
+                .map((alb) => (
+                  <button
+                    key={alb.Combined_Key}
+                    className=" col-4-sm app"
+                    onClick={() => handleSelectAlbum(alb.Combined_Key)}
+                  >
+                    <div className="icons">
+                      <p>{alb.Country_Region}</p>
+                      <p>{alb.Case_Fatality_Ratio}</p>
+                    </div>
+                    <i className="icon-wrapper">
+                      <FontAwesomeIcon icon={faArrowRight} />
+                    </i>
+                  </button>
+                ))}
+            </div>
+          </div>
+        )}
+      </section>
+    );
   }
-
-
-  const handleSubmit = () => {
-    // setValue(search)
-    setSearch('')
-  }
-
-  return (
-    <section>
-        <div className="form">
-        <form onSubmit={handleSubmit} className="search-box">
-            <input  value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Country"/>
-            <button type="submit">Search</button>
-        </form>
-        </div>
-      <div className="row">
-        {album.filter(album =>album.Country_Region.toLowerCase().includes(search)).map((alb) => (
-        <button key={alb.Combined_Key} className=" col-4-sm app">
-            <div className="icons">
-        <p>{alb.Country_Region}</p>
-        <p>{alb.Case_Fatality_Ratio}</p>
-        </div>
-       
-        <i className="icon-wrapper">
-          <FontAwesomeIcon icon={faArrowRight} />
-        </i>
-        
-        </button>
-        ))}
-      </div>
-    </section>
-  );
-}
+  
 
 export default Home;
